9ffbeca0cdb6a30efd0aa76fa7a90bef1b05db54
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2017-01-06 12:30:59 +0100

    Added keys to responses

--- src/server.js
+++ src/server.js
@@ -8,27 +8,27 @@ export function createServer(connection, dbName) {
   const router = koaRouter()
     .get('/rest/cities/', async(ctx) => {
       const cities = await getCities(connection, dbName);
-      ctx.body = JSON.stringify(cities);
+      ctx.body = JSON.stringify({cities});
     })
     .get('/rest/cities/:id/', async(ctx) => {
       const city = await getCity(connection, dbName, ctx.params.id);
-      city ? ctx.body = JSON.stringify(city) : ctx.status = 404;
+      city ? ctx.body = JSON.stringify({city}) : ctx.status = 404;
     })
     .get('/rest/stations/', async(ctx) => {
       const stations = await getStations(connection, dbName);
-      ctx.body = JSON.stringify(stations);
+      ctx.body = JSON.stringify({stations});
     })
     .get('/rest/stations/:id/', async(ctx) => {
       const station = await getStation(connection, dbName, ctx.params.id);
-      station ? ctx.body = JSON.stringify(station) : ctx.status = 404;
+      station ? ctx.body = JSON.stringify({station}) : ctx.status = 404;
     })
     .get('/rest/sensors/:stationId/', async(ctx) => {
       const sensors = await getSensors(connection, dbName, ctx.params.stationId);
-      ctx.body = JSON.stringify(sensors);
+      ctx.body = JSON.stringify({sensors});
     })
     .get('/rest/sensors/:id/readings/', async(ctx) => {
-      const sensors = await getReadings(connection, dbName, ctx.params.id);
-      ctx.body = JSON.stringify(sensors);
+      const readings = await getReadings(connection, dbName, ctx.params.id);
+      ctx.body = JSON.stringify({readings});
     });
 
   const app = new Koa();
